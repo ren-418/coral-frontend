27e135931d4071e665be407edcfcbd4119a5f6d6
ren-418 <mrsteevtvw418@gmail.com>
2024-04-04 00:22:14 -0300

    HandleErrors inputs and collect data from input

--- src/app/pages/register/RegisterPage.jsx
+++ src/app/pages/register/RegisterPage.jsx
@@ -1,10 +1,73 @@
 import React from 'react'
+import { useState } from 'react';
 import './RegisterPage.scss'
 import { Link } from 'react-router-dom';
 import ModernInput from '../../../components/modern inputs/ModernInput';
 import ClassicInput from '../../../components/classic input/ClassicInput';
 
 function RegisterPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [accountType, setAccountType] = useState("");
+
+  const [errors, setErrors] = useState({
+    email: '',
+    password: '',
+    confirmPassword: '',
+    userType: ''
+  });
+
+
+  /*const onSubmit = async () => {
+    setLoading(true);
+
+    try {
+      const res = await fetch('http://localhost:8080/api/v1/auth/register', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          email: email,
+          password: password
+        }),
+      });
+
+      setLoading(false);
+
+      if (!res.ok) {
+        const error = await res.text();
+        handleError(error);
+      } else {
+        const message = await res.text();
+        console.log(message);
+        navigate('/login');
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };*/
+
+  function test(){
+    const newErrors = {};
+    if (!email.includes('@')) {
+      newErrors.email = 'Email inválido';
+    }
+    if (password.length < 8) {
+      newErrors.password = 'La contraseña debe tener al menos 8 caracteres';
+    }
+    if (password !== confirmPassword) {
+      newErrors.confirmPassword = 'Las contraseñas no coinciden';
+    }
+    if(accountType === ""){
+      newErrors.userType = 'Debes seleccionar un tipo de cuenta';
+    }
+
+    setErrors(newErrors);
+  }
+
   return (
     <>
     <div className="register-page">
@@ -12,13 +75,13 @@ function RegisterPage() {
       <h1>Register</h1>
       <div className='form-container'>
         <div className='check-container'>
-          <label>Investor or enterprise?</label>
-          <ClassicInput type='select' placeholder='Select account type' options={["Enterprise", "Investor"]}/>
+          <label>User type</label>
+          <ClassicInput type='select' placeholder='Select account type' options={["Enterprise", "Investor"]} onChange={setAccountType} errorMessage={errors.userType}/>
         </div>
-        <ModernInput type="text" color="white">Email</ModernInput>
-        <ModernInput type="password" color="white">Password</ModernInput>
-        <ModernInput type="password" color="white">Repeat Password</ModernInput>
-        <button className='button'>Register</button>
+        <ModernInput type="text" color="white" onChange={setEmail} errorMessage={errors.email}>Email</ModernInput>
+        <ModernInput type="password" color="white" onChange={setPassword} errorMessage={errors.password}>Password</ModernInput>
+        <ModernInput type="password" color="white" onChange={setConfirmPassword} errorMessage={errors.confirmPassword}>Repeat Password</ModernInput>
+        <button className='button' onClick={test}>Register</button>
       </div>
       <div className='login'>
         <p>Already have an account?</p>
--- src/components/modern inputs/ModernInput.jsx
+++ src/components/modern inputs/ModernInput.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import './ModernInput.scss'
 
-function ModernInput({children, type, color, onChange}) {
+function ModernInput({children, type, color, onChange, errorMessage}) {
   return (
-    <div className="input-container">
-        <input type={type} style={{color: color, borderBottom: '2px solid '+color}} required onChange={onChange}/>
-        <label style={{color: color}}>{children}</label>
+    <div className='modern-input-container'>
+    <div className="modern-input">
+      <input type={type} style={{color: color, borderBottom: '2px solid '+color}} required onChange={(element)=>{onChange(element.target.value)}}/>
+      <label style={{color: color}}>{children}</label>
     </div>
+    {errorMessage && <span>{errorMessage}</span>}
+    </div>
+
   )
 }
 

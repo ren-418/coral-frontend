ad1f8dac22c7261388cbdc25b2b4857ca4acd0a4
ren-418 <mrsteevtvw418@gmail.com>
2024-04-30 11:56:26 -0300

    last minute changes before deadline

--- src/app/Controller.jsx
+++ src/app/Controller.jsx
@@ -4,6 +4,8 @@ import React, { useState, useEffect } from 'react'
 import Template from './templates/app template/template'
 import { Link, useNavigate } from 'react-router-dom';
 import Profile from './pages/profile/Profile';
+import CreateInvestor from './pages/create investor/CreateInvestor';
+import CreateEnterprise from './pages/create enterprise/CreateEnterprise';
 
 function Controller() {
     const routes = {
@@ -12,7 +14,9 @@ function Controller() {
         add: 2,
         messages: 3,
         profile: 4,
-        news: 5
+        news: 5,
+        editInvestor: 6,
+        editEnterprise: 7
     };
 
     const [page, setPage] = useState(routes.home)
@@ -52,6 +56,7 @@ function Controller() {
         const user = await res.json();
         if(res.ok){
           setUser(user);
+          console.log(user)
 
           if(user.firstLogin === true){
             if(user.userType === "InvestorUser"){
@@ -80,8 +85,10 @@ function Controller() {
         {page === routes.search && <Template selected={page} setPage={setPage} userType={user.userType}>{ user.userType === "InvestorUser" ? <SearchInvestors/> : <SearchInvestors/>}</Template>}
         {page === routes.add && <Template selected={page} setPage={setPage} userType={user.userType}> <Home/></Template>}
         {page === routes.messages && <Template selected={page} setPage={setPage} userType={user.userType}><Home/></Template>}
-        {page === routes.profile && <Template selected={page} setPage={setPage} userType={user.userType}><Profile/></Template>}
+        {page === routes.profile && <Template selected={page} setPage={setPage} userType={user.userType}><Profile setPage={setPage} userType={user.userType}/></Template>}
         {page === routes.news && <Template selected={page} setPage={setPage} userType={user.userType}><Home/></Template>}
+        {page === routes.editInvestor && <Template selected={routes.profile} setPage={setPage} userType={user.userType}><CreateInvestor nameP={user.name} aboutMeP={user.description} invCriteriaP={user.investmentCriteria} countryP={user.location} invMinP={user.rangeMin} invMaxP={user.rangeMax} investorTypeP={user.investorType} areaListP={user.areas} imageBlobP={user.profilePicture} firstLogin={user.firstLogin}/></Template>}
+        {page === routes.editEnterprise && <Template selected={routes.profile} setPage={setPage} userType={user.userType}><CreateEnterprise/></Template>}
     </>
   )
 }
